eea704c5b87ab02cca37853d587d411626ec2c77
Anatolii <144040851+Anatolii-Ovcharuk@users.noreply.github.com>
2024-10-09 10:39:14 +0200

    Add component

---
+++ Sprite component for SVG/Sprite.js
@@ -0,0 +1,15 @@
+/* "Sprite component", v. 0.5 - 10.10.2024 | MIT License | Made by Anatolii Ovcharuk - https://github.com/Anatolii-Ovcharuk */
+/* Description: This is Sprite Component for React. */
+/* Example for use in application: <Sprite id="logo" /> */
+
+import React from 'react';
+import sprite from './Sprite.svg';
+
+const Sprite = ({ id }) => (
+  <svg className={id}>
+    <use href={`${sprite}#${id}`} ></use>
+    {/* <use xlinkHref={`${sprite}#${id}`}></use> */}
+  </svg>
+);
+
+export default Sprite;
