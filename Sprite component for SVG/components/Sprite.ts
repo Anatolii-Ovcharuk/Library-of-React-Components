25d9698a304b50fb452e225e33d17854c0eed5eb
Anatolii <144040851+Anatolii-Ovcharuk@users.noreply.github.com>
2024-11-08 00:28:43 +0100

    Create Sprite.ts

---
+++ Sprite component for SVG/components/Sprite.ts
@@ -0,0 +1,19 @@
+/* "Sprite component", v. 0.5 - 10.10.2024 | MIT License | Made by Anatolii Ovcharuk - https://github.com/Anatolii-Ovcharuk */
+/* Description: This is Sprite Component for React. */
+/* Example for use in application: <Sprite id="logo" /> */
+
+import React from 'react';
+import sprite from './Sprite.svg';
+
+interface SpriteProps {
+  id: string;
+}
+
+const Sprite: React.FC<SpriteProps> = ({ id }) => (
+  <svg className={id}>
+    <use href={`${sprite}#${id}`}></use>
+    {/* <use xlinkHref={`${sprite}#${id}`}></use> */}
+  </svg>
+);
+
+export default Sprite;
